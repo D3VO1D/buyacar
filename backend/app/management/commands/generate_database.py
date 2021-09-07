ad8248928ec7465f7b6ac4e17041b5e3c56fe8b6
UZh1k <danyanygin4@gmail.com>
2021-09-07 23:16:48 +0300

    db corrections

--- backend/app/management/commands/generate_database.py
+++ backend/app/management/commands/generate_database.py
@@ -10,18 +10,6 @@ class Command(BaseCommand):
               python manage.py generate_database --num_rows=100 --num_photos=5 or 
               python manage.py generate_database --num_photos=5"""
 
-    conditions = {'new': 'new',
-                  'excellent': 'new',
-                  'very good': 'used',
-                  'good': 'used',
-                  'fair': 'used',
-                  'used': 'used',
-                  'cpo': 'used',
-                  'for parts': 'damaged',
-                  'damaged': 'damaged',
-                  '': ''
-                  }
-
     def add_arguments(self, parser):
         parser.add_argument("--num_rows", type=int, help="Number of rows called from csv")
         parser.add_argument("--num_photos", type=int, help="Maximum number of photos for ad")
@@ -29,15 +17,19 @@ class Command(BaseCommand):
     def handle(self, *args, **kwargs):
         try:
             num_rows = kwargs['num_rows']
-            cars = pd.read_csv("ads.csv", nrows=num_rows, delimiter="|")
+            cars = pd.read_csv("ads.csv", nrows=num_rows, delimiter=";")
         except:
-            cars = pd.read_csv("ads.csv", delimiter="|")
+            cars = pd.read_csv("ads.csv", delimiter=";")
 
         num_photos = kwargs['num_photos']
 
         for index, car in cars.iterrows():
+            if car.make == "nan" and car.model == "nan":
+                continue
             new_car_ad = CarAdvertisement.objects.create(source=car.source,
                                                          url=car.url,
+                                                         is_new=True if car.is_new == "1" else False,
+                                                         is_broken=car.is_broken,
                                                          price=car.price,
                                                          location=car.location,
                                                          latitude=car.latitude,
@@ -52,6 +44,5 @@ class Command(BaseCommand):
                                                          mileage=car.mileage,
                                                          transmission=car.transmission,
                                                          drive=car.drive,
-                                                         condition=self.conditions[car.condition.lower()],
                                                          power=car.power
                                                          )
